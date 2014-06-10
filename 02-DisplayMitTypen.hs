677aec5d990bf1d4e3ce33dd080c151988ec6fa5
Fronx <fronx@soundcloud.com>
2014-06-11 00:48:09 +0200

    more

---
+++ 02-DisplayMitTypen.hs
@@ -0,0 +1,21 @@
+import Graphics.Gloss
+
+bildschirm :: Display
+bildschirm = FullScreen (320, 200)
+
+hintergrund :: Color
+hintergrund = white
+
+kreis1 :: Picture
+kreis1 = circle 100
+
+kreis2 :: Picture
+kreis2 = translate 40 30 (color red kreis1)
+
+main :: IO ()
+main = display bildschirm hintergrund
+  (pictures
+    [ kreis1
+    , kreis2
+    ]
+  )
